de55109497f7d0a27c6e350cb04712b0d9cadf97
Navjot10 <83138142+Navjot10@users.noreply.github.com>
2024-08-06 01:18:12 -0400

    Update background.js

--- src/background.js
+++ src/background.js
@@ -2,9 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "GET_SUMMARY") {
     let videoId = message.videoId;
     console.log(`Received video ID: ${videoId}`);
-    fetchTranscript(videoId).then(transcript => {
-      return getSummary(transcript);
-    }).then(summary => {
+    getSummary(videoId).then(summary => {
       sendResponse({summary: summary});
     }).catch(error => {
       console.error("Error in GET_SUMMARY listener:", error);
@@ -14,33 +12,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-async function fetchTranscript(videoId) {
+async function getSummary(videoId) {
   try {
-    console.log(`Fetching transcript for video ID: ${videoId}`);
-    let response = await fetch(`http://localhost:5001/get_transcript?video_id=${videoId}`);
-    let data = await response.json();
-    if (data.error) {
-      console.error("Error fetching transcript:", data.error);
-      return "Error fetching transcript.";
-    }
-    console.log(`Transcript: ${data.transcript}`);
-    return data.transcript;
-  } catch (error) {
-    console.error("Error fetching transcript:", error);
-    return "Error fetching transcript.";
-  }
-}
+    console.log("Fetching summary from Flask server");
 
-async function getSummary(transcript) {
-  try {
-    console.log("Fetching summary from Node.js server");
-
-    let response = await fetch('http://localhost:5002/get_summary', {
-      method: 'POST',
+    let response = await fetch(`http://localhost:5001/get_summary?video_id=${videoId}`, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ transcript })
+      }
     });
 
     if (!response.ok) {
